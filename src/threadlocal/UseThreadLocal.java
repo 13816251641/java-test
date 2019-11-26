ec2b2ef7f88cd3364abb08e92bedc160dd7fe45e
admin <982584989@qq.com>
2019-11-26 22:23:28 +0800

    代码提交

---
+++ src/threadlocal/UseThreadLocal.java
@@ -0,0 +1,25 @@
+package threadlocal;
+
+/**
+ * @Auther ljn
+ * @Date 2019/11/26
+ * 使用ThreadLocal类
+ */
+public class UseThreadLocal {
+
+    public static void main(String[] args) {
+        /*
+            ThreadLocal的set方法默认会创建一个ThreadLocalMap类,
+            ThreadLocalMap类是ThreadLocal的静态内部类,然后将这个
+            内部类赋值到Thread即当前线程的 ThreadLocal.ThreadLocalMap threadLocals
+            中去,这是创建ThreadLocalMap的过程,一个线程共用这一个
+            ThreadLocalMap类,它的key是每个ThreadLocal的实例!!!
+         */
+        ThreadLocal<String> tl = new ThreadLocal<>();
+        tl.set("av");
+
+
+
+    }
+
+}
