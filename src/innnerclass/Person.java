0df6b88f02d3850437419b5af8cb2fb675fd0804
admin <982584989@qq.com>
2020-01-02 10:35:08 +0800

    代码提交

--- src/innnerclass/Person.java
+++ src/innnerclass/Person.java
@@ -4,25 +4,25 @@ package innnerclass;
  * Person是外部类,Hope是静态内部类
  */
 public class Person {
-    private int age =27;
+    public int age =27;
+    private static String out="a";
+    static {
+        System.out.println("Person's static code");
+    }
     public Person(){
         System.out.println("Person");
     }
-
     static class Hope {
         static {
-            System.out.println("Hope's inner code");
+            System.out.println("Hope's static code");
         }
         private String name="ch";
         public Hope(){
             System.out.println("Hope");
         }
         public String getName(){
-            return this.name;
+            System.out.println(out);
+            return "a";
         }
     }
-
-
-
-
 }
--- src/innnerclass/TestPerson.java
+++ src/innnerclass/TestPerson.java
@@ -1,30 +1,41 @@
 package innnerclass;
 
+import org.junit.Test;
+
 /**
- * 测试静态内部类
+ * 测试静态内部类,静态内部类本质和外部类联系不大
  */
 public class TestPerson {
+    /*
+      创建外部类的时候不会自动创建静态内部类实例
+     */
+    @Test
+    public void test1(){
+        Person person= new Person();
+    }
 
-    public static void main(String[] args) {
+    @Test
+    public void test2(){
         /*
-           创建外部类的时候不会自动创建静态内部类实例
-         */
-        //Person person= new Person();
+           2个对象不相等,创建静态内部类不会构造外部类
+           对象(不会执行外部类的static代码和构造代码),
+           静态内部类的静态代码也只会执行一次
+        */
+       Person.Hope hope1 = new Person.Hope();
+       Person.Hope hope2 = new Person.Hope();
+       System.out.println(hope1 == hope2);
+    }
 
-        /*
-            2个对象不相等,创建静态内部类不会构造外部类对象
-            静态代码块的代码只会执行一次
-         */
-         /*
-           Person.Hope hope1 = new Person.Hope();
-           Person.Hope hope2 = new Person.Hope();
-           System.out.println(hope1 == hope2);
-         */
-
+    /*
+       静态内部类可以直接访问外部类的静态变量,即使它
+       在外部类中是私有的,一旦访问外部类的静态资源,
+       外部类的静态代码块,静态变量都会得到初始化(原本是没有的)
+     */
+    @Test
+    public void test3(){
         Person.Hope hope = new Person.Hope();
         String name = hope.getName();
         System.out.println(name);
-
     }
 
 }
