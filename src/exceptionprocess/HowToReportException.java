e934008208b8da7a7f90cdce651a1f66dcf37157
admin <982584989@qq.com>
2020-01-03 12:39:06 +0800

    代码提交

--- src/exceptionprocess/HowToReportException.java
+++ src/exceptionprocess/HowToReportException.java
@@ -2,6 +2,8 @@ package exceptionprocess;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.Arrays;
+
 /**
  * @Auther ljn
  * @Date 2020/1/1
@@ -13,12 +15,29 @@ public class HowToReportException {
     private static int m = 0;
 
     public static void main(String[] args) {
-         System.out.println("test exception");
-         try{
+        Object[] checkItems  = {"12306","98275"};
+        Sample sample = new Sample();
+        sample.age = 29;
+        sample.name = "lujieni";
+        try{
              m = 899 / 0;
          }catch (Exception e){
-            log.error("出现异常了!!!",e);
-         }
+            //log.error("出现异常了!!! 异常数据为:"+ Arrays.toString(checkItems),e);
+            /* 需要重写sample的toString方法 */
+            log.error("出现异常了!!! 异常数据为:"+ sample,e);
+        }
+
+    }
+
+    static class Sample{
+        private int age;
+        private String  name;
+
+        @Override
+        public String toString(){
+           return "age:"+age+";name:"+name;
+        }
 
     }
 }
+
