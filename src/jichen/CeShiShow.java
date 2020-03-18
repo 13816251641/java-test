7831c6f4e33c70813eb0b2cbc8ec68a912420790
lujieni <lujieni1017>
2020-03-18 11:18:34 +0800

    代码提交

--- src/jichen/CeShiShow.java
+++ src/jichen/CeShiShow.java
@@ -1,16 +1,15 @@
 package jichen;
 
 /**
- * 子类调用父类的方法,需要打印this.getClass().getName()的时候
- * 返回的包名+类名仍旧是子类
+ * 子类调用父类的方法,需要打印this.getClass().getName()的时候返回的包名+类名仍旧是子类
  */
 public class CeShiShow {
 	public static void main(String[] args) {
           Father f = new Son();
-          /*子类覆写了父类的toString方法,所以这里调用的子类的方法,但执行类是jichen.Son 子类*/
+          /*子类覆写了父类的show方法,所以这里调用的子类的方法,执行类是jichen.Son 子类*/
           System.out.println(f.show());
           Father f1 = new Father();
-          /*调用父类自己的toString方法,执行类是jichen.Father类*/
+          /*调用父类自己的show方法,执行类是jichen.Father类*/
           System.out.println(f1.show());
 	}
 }
@@ -28,6 +27,9 @@ class Father {
 	子类
  */
 class Son extends Father {
+	/*
+		调用父类的方法
+	 */
 	@Override
 	public String show() {
 		return super.show();
--- src/jichen/Person.java
+++ src/jichen/Person.java
@@ -9,7 +9,7 @@ public class Person {
     }
 
     public static void main(String[] args) {
-        /*这里new了一个匿名内部类,其实也是Person的子类*/
+        /*这里new了一个匿名内部类,这个匿名内部类是Person类的子类*/
         Person p = new Person(){
             @Override
             public void show(){
