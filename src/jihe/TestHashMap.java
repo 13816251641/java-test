93e300298ed8a67c45b0c986a2def1ace0c86362
13816251641 <lujieni1017>
2020-04-02 12:51:20 +0800

    代码提交

--- src/jihe/TestHashMap.java
+++ src/jihe/TestHashMap.java
@@ -1,22 +1,35 @@
 package jihe;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
 
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 public class TestHashMap {
     /*
        String重写了equals和hashcode方法,所以可以使用String当做key来去重,
        这样效率会更高
      */
     @Test
-    public void testOne(){
-        Map<String,Integer> map = new HashMap<>();
-        map.put("123",1);
+    public void testOne() {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("123", 1);
         System.out.println(map.containsKey("123"));
     }
 
 
+    /*
+        alt+f8 可以在debugger的时候查看变量的值并进行修改!!!
+     */
+    @Test
+    public void testTwo(){
+        int a = 11;
+        Map<String, Object> map = new HashMap<>();
+        map.put("name","Pi");
+        map.put("age",20);
+        System.out.println(map);
+    }
 }
