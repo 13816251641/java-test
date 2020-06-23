23eb7326261948adc4aee5d8ac2cb203c104d3d4
181268 <lujieni1017>
2020-06-23 10:04:40 +0800

    代码提交

--- src/duixiangleixin/TestInteger.java
+++ src/duixiangleixin/TestInteger.java
@@ -17,18 +17,18 @@ public class TestInteger {
         Integer i = new Integer(128);
         Integer i2 = 128;
 
-        System.out.println(i == i2);
+        System.out.println(i == i2);//false
 
         Integer i3 = new Integer(127);
         Integer i4 = 127;
-        System.out.println(i3 == i4);
+        System.out.println(i3 == i4);//false
 
         Integer i5 = 128;
         Integer i6 = 128;
-        System.out.println(i5 == i6);
+        System.out.println(i5 == i6);//false
 
         Integer i7 = 127;
         Integer i8 = 127;
-        System.out.println(i7 == i8);
+        System.out.println(i7 == i8);//true
     }
 }
