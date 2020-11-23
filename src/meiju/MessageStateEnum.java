f2d17f66dc18f2559a47c23e2c897ed9c0e06e6e
181268 <lujieni1017>
2020-11-23 10:39:19 +0800

    代码提交

--- src/meiju/MessageStateEnum.java
+++ src/meiju/MessageStateEnum.java
@@ -2,6 +2,8 @@ package meiju;
 
 import lombok.Getter;
 
+import java.util.Objects;
+
 /**
  * 枚举类名驼峰即可,枚举类名建议带上Enum后缀
  */
@@ -9,17 +11,20 @@ import lombok.Getter;
 public enum MessageStateEnum {
 
     /*里面的字段名一律大写,单词之间下划线分割*/
-    OK("ok",11),
-    ERROR("error",12);
+    OK_ELEMENT("ok",11,"a"),
+    ERROR_ELEMENT("error",12,"b");
 
     private final String status;
 
     private final int num;
 
+    private final String content;
+
     /*构造方法被默认强制是私有*/
-    MessageStateEnum(String status,Integer num){
+    MessageStateEnum(String status,Integer num,String content){
         this.status=status;
         this.num = num;
+        this.content = content;
     }
 
     /**
@@ -34,6 +39,22 @@ public enum MessageStateEnum {
             }
         }
         //默认风格
+        return null;
+    }
+
+    /**
+     *
+     * @param num
+     * @return
+     *
+     * 输入num 输出String
+     * num => status
+     */
+    public static String getStatus(Integer num) {
+        for(MessageStateEnum element : MessageStateEnum.values()){
+            if(Objects.equals(num,element.getNum()))
+                return element.getStatus();
+        }
         return null;
     }
 }
--- src/meiju/TestMeiJu.java
+++ src/meiju/TestMeiJu.java
@@ -1,27 +1,34 @@
 package meiju;
 
 /**
-        * 测试枚举类
-        *
-        */
+ * 测试枚举类
+ */
 public class TestMeiJu {
 
     public static void main(String[] args) {
-        MessageStateEnum a = MessageStateEnum.OK;
-        needEnum(a);
+        MessageStateEnum a = MessageStateEnum.OK_ELEMENT;
+        //needEnum(a);
         MessageStateEnum b = MessageStateEnum.toEnum("error");
-        System.out.println(b);//ERROR
+        System.out.println(b);//ERROR_ELEMENT
+        String status = MessageStateEnum.getStatus(11);
+        System.out.println(status);//ok
     }
 
+
+
     private static void needEnum(MessageStateEnum mse){
-        System.out.println(mse);//OK->String类型
+        System.out.println(mse);//OK_ELEMENT (枚举变量的名称) -> String类型
         System.out.println(mse.getNum());//11
         System.out.println(mse.getStatus());//ok
-        /*枚举对象的==和equals方式是一个作用看源码可知*/
-        if(mse == MessageStateEnum.OK){
+        /* 枚举对象的==和equals方式是一个作用看源码可知 */
+        if(mse == MessageStateEnum.OK_ELEMENT){
             System.out.println("true");
         }else{
             System.out.println("false");
         }
     }
+
+
+
+
 }
