278b64eb5b0daa95484534a7325cafb2e55f5f6c
181268 <lujieni1017>
2021-01-26 15:27:59 +0800

    代码提交

--- src/stream/UseFlatMap.java
+++ src/stream/UseFlatMap.java
@@ -1,12 +1,7 @@
 package stream;
 
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 import org.junit.Test;
-import org.springframework.core.env.SystemEnvironmentPropertySource;
-import yufaxijie.duixiangcopy.Person;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -25,15 +20,12 @@ public class UseFlatMap {
     @Test
     public void test1(){
         String[] words = new String[]{"Hello","World"};
-        List<String[]> result = Arrays.stream(words).map(word -> word.split("")).distinct().collect(Collectors.toList());
-        result.forEach(e->{
-            System.out.println(Arrays.toString(e));
-        });
-    }
-
-    @Test
-    public void test2(){
-        String[] words = new String[]{"Hello","World"};
+        /*
+            1. e -> e.split("")  分别将"Hello" & "World" -> String[]  String[]
+            2. e -> {            e是上面的String[]
+                 return Arrays.stream(e);
+               }
+         */
         List<String> result = Arrays.stream(words).map(e -> e.split("")).flatMap(e -> {
             return Arrays.stream(e);
         }).distinct().collect(Collectors.toList());
@@ -41,61 +33,43 @@ public class UseFlatMap {
     }
 
     @Test
-    public void test3(){
+    public void test2(){
        String content = "abcde";
-        String[] split = content.split("");
-        System.out.println(Arrays.toString(split));
-    }
-
-    @Test
-    public void test4(){
-        String[] words = new String[]{"Hello","World"};
-        List<String> collect = Arrays.stream(words).flatMap(e -> {
-            String[] split = e.split("");
-            return Arrays.stream(split);
-        }).collect(Collectors.toList());
-        System.out.println(collect);
+       String[] split = content.split("");
+       System.out.println(Arrays.toString(split));
     }
 
     /*
-        如果list中的item又包含了list,你想要得到所有并放入一个
-        list中,那么就要使用flatMap
+        如果list中的item又包含了list,你想要得到所有并放入一个list中,那么就要使用flatMap
      */
     @Test
-    public void test5(){
+    public void test3(){
       List<DataCarrier> list = new ArrayList<>();
       DataCarrier a = new DataCarrier();
-      a.setList(Arrays.asList("a","b"));
+      a.setList(Arrays.asList("a","b","c"));
       DataCarrier b = new DataCarrier();
       b.setList(Arrays.asList("1","2"));
       list.add(a);
       list.add(b);
 
-/*      List<String> result = list.stream().map(e -> e.getList()).flatMap(e -> e.stream()).collect(Collectors.toList());
-      System.out.println(result);*/
+      /*
+          为了得到 a,b,c,1,2
+       */
+      //方法1
+      List<String> result1 = list.stream().map(e -> e.getList()).flatMap(e -> e.stream()).collect(Collectors.toList());
+      System.out.println(result1);
 
-        List<String> result = list.stream().flatMap(e -> {
-            return e.getList().stream().filter(g->{
-               if(g.equals("1")){
-                    return true;
-                }
-               else{
-                    return false;
-                }
-            });
-        }).collect(Collectors.toList());
-        System.out.println(result);
-
+      //方法2
+      List<String> result2 = list.stream().flatMap(e -> {
+         return e.getList().stream();
+      }).collect(Collectors.toList());
+      System.out.println(result2);
 
     }
-
 }
 
 @Data
 class DataCarrier{
     private List<String> list;
-
-
-
 }
 
