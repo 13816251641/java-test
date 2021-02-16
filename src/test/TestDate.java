ddc52f0bb3c11d6a71d96a8eba462fcc59214f17
181268 <lujieni1017>
2021-02-16 19:55:18 +0800

    代码提交

---
+++ src/test/TestDate.java
@@ -0,0 +1,22 @@
+package test;
+
+import org.junit.Test;
+
+import java.util.Date;
+
+/**
+ * @Package: test
+ * @ClassName: TestDate
+ * @Author: lujieni
+ * @Description: 1
+ * @Date: 2021-02-05 16:31
+ * @Version: 1.0
+ */
+public class TestDate {
+
+    @Test
+    public void test01(){
+        System.out.println(new Date(1612515086400L));
+
+    }
+}
