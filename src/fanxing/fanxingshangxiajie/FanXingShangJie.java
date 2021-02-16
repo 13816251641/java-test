1628c526da5b4a05e3d70a3b02ffb81147568633
181268 <lujieni1017>
2021-02-16 20:02:01 +0800

    代码提交

--- src/fanxing/fanxingshangxiajie/FanXingShangJie.java
+++ src/fanxing/fanxingshangxiajie/FanXingShangJie.java
@@ -18,13 +18,13 @@ public class FanXingShangJie<T> {
     }
 
     public static void main(String[] args) {
-            /*泛型的上界*/
-            FanXingShangJie<Integer> origin = new FanXingShangJie<>();
-            origin.setFirst(28);
-            FanXingShangJie<? extends Number> after = origin;
-            /*after.setFirst(13L);不能调用function(T t)这样的方法*/
-            Number first = after.getFirst();
-            System.out.println(first);
+        /*泛型的上界*/
+        FanXingShangJie<Integer> origin = new FanXingShangJie<>();
+        origin.setFirst(28);
+        FanXingShangJie<? extends Number> after = origin;
+        /*after.setFirst(13L);不能调用function(T t)这样的方法*/
+        Number first = after.getFirst();
+        System.out.println(first);
     }
 }
 
