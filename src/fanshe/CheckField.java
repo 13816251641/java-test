4a3a5dcf66efb83d6f91be6a8222c892c06eed95
13816251641 <982584989@qq.com>
2021-03-01 22:32:32 +0800

    代码提交

--- src/fanshe/CheckField.java
+++ src/fanshe/CheckField.java
@@ -6,7 +6,7 @@ import java.lang.reflect.Field;
 
 /**
  * 利用反射读取字段值
- * getFields()：获得某个类的所有的公共（public）的字段，包括父类中的字段。
+ * getFields()：获得某个类的所有的公共（public）的字段，包括父类中公共（public）字段。
  * getDeclaredFields()：获得某个类的所有声明的字段，即包括public、private和proteced，但是不包括父类的申明字段。
  */
 public class CheckField {
@@ -20,7 +20,7 @@ public class CheckField {
 
 	public static void testGetFieldValueByReflect(Object model) throws Exception {
 		//System.out.println(model.getClass().getName());fanshe.Employee
-		for (Field field : model.getClass().getDeclaredFields()) {
+		for (Field field : model.getClass().getFields()) {
 			boolean flag = field.isAccessible();
 			field.setAccessible(true);
 			System.out.println(field.getName() + ":" + field.get(model));
@@ -32,14 +32,13 @@ public class CheckField {
 
 @Data
 class Person{
-	private String name;
+	public String name;
 	
 	public Person(String name) {
-		this.name=name;
+		this.name = name;
 	}
 }
 
-@Data
 class Employee extends Person{
 	
 	private double salary;
